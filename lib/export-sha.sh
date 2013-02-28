5898e189de76bd4b486f90d6c533d4b46ed5eb9b
Corey Frang <gnarf@gnarf.net>
2013-02-28 01:02:38 -0600

    Change the export sha script to use a clone/checkout to be able to submodule

--- lib/export-sha.sh
+++ lib/export-sha.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
-git archive $1 | tar -x -C $2
+bare=$(pwd)
+dest=$2
+sha=$1
+
+if [ -d $dest ]
+then
+	if [ ! -d $dest/.git ]
+	then
+		rm -rf $dest
+		git clone $bare $dest
+	fi
+else
+	git clone $bare $dest
+fi
+cd $dest
+git fetch
+git checkout --force $sha
+git reset --hard $sha
+git submodule update --init
